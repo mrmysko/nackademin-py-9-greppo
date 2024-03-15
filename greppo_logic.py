d9adc2e8730e70fb29764ef0b962b853a4486e65
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-15 11:43:52 +0100

    Working on invert...again

--- greppo_logic.py
+++ greppo_logic.py
@@ -1,7 +1,7 @@
 # Todo - Error-handling
 # Todo - Less ifs
 # Todo - Only match actual search_terms, not "for" in "fortnite" - DONE
-# Todo - Invert seems to match empty lines.
+# Todo - Invert seems to match empty lines. - FIXED
 
 """Do I need to collect all the lines, and then iterate AGAIN over matched lines and remove them?
         Or maybe do a key,value pair updating 0/1 if the word is found and then save the line if either any are found, or invert none is."""
@@ -12,75 +12,45 @@ import re
 def greppo_logic(search_terms, filenames, invert_match: bool, show_line_numbers: bool):
     """Returns lines matching items in search_terms."""
 
-    search_word_dict = dict((i, False) for i in search_terms)
     match_lines = list()
 
     for file in filenames:
         with open(file, "r", encoding="utf-8") as fp:
             reader = fp.readlines()
 
-            for line in reader:
+            """OR...save filenames, and matches as separate strings, and if show_line_numbers is True, save that too.
+            Then just do a ''.join() on those three parts and append to the list. Then I could cut down the code for line numbers."""
 
-                # Reset search_word tracker.
-                search_word_dict = dict((i, 0) for i in search_terms)
+            if invert_match:
+                for index, line in enumerate(reader):
 
-                # Iterate over keys, looking for matches. If a match is found, flip that key to True.
-                for key, value in search_word_dict.items():
-                    if key in line:
-                        search_word_dict[key] = True
+                    # Reset search_word tracker.
+                    search_word_dict = dict((i, False) for i in search_terms)
 
-                # Returns False if any value in search_word_dict is True.
-                if not any(search_word_dict.values()) and len(line) > 1:
-                    match_lines.append(rf"{file}:{line.strip()}")
-                else:
-                    pass
-                """Now, hur kokar jag ner den här i en list comprehension?"""
+                    for key in search_word_dict.keys():
+                        # Returns a match object. True if a match is found.
+                        if re.search(rf"\b{key}\b", line):
+                            search_word_dict[key] = True
 
-                # print(search_word_dict)
+                        # Returns False if any value in search_word_dict is True. Check length of string to not add empty strings. (\n counts as a char)
+                        if not any(search_word_dict.values()) and len(line) > 1:
+                            match_lines.append(rf"{file}:{index}:{line.strip()}")
+                            """Now, hur kokar jag ner den här i en list comprehension?"""
 
-    print(match_lines)
+            # This is not readable tbh.                
+            # If note invert match, easier.
+            else:
+                match_lines.extend(
+                    [
+                        f"{file}:{str(index) + ":" if show_line_numbers == True else ''}{line.strip('\n')}"
+                        for x in search_terms
+                        for index, line in enumerate(reader)
+                        if re.search(rf"\b{x}\b", line)
+                    ]
+                )
 
-    # This is not readable tbh.
-    # When invert looks for lines not containing "for", it matches lines containing "banan"...
-    # but then when it looks for not "banan", it matches lines containing "for".
-    # """if invert_match:
-    #    if show_line_numbers:
-    #        match_lines.extend(
-    #            [
-    #                f"{file}:{index}:{line.strip('\n')}"
-    #                for x in search_terms
-    #                for index, line in enumerate(reader)
-    #                if not re.search(rf"\b{x}\b", line)
-    #            ]
-    #        )
-    #    else:
-    #        match_lines.extend(
-    #            [
-    #                f"{file}:{line.strip('\n')}"
-    #                for x in search_terms
-    #                for line in reader
-    #                if not re.search(rf"\b{x}\b", line)
-    #            ]
-    #        )
-    # else:
-    #    if show_line_numbers:
-    #        match_lines.extend(
-    #            [
-    #                f"{file}:{index}:{line.strip('\n')}"
-    #                for x in search_terms
-    #                for index, line in enumerate(reader)
-    #                if re.search(rf"\b{x}\b", line)
-    #            ]
-    #        )
-    #    else:
-    #        match_lines.extend(
-    #            [
-    #                f"{file}:{line.strip('\n')}"
-    #                for x in search_terms
-    #                for line in reader
-    #                if re.search(rf"\b{x}\b", line)
-    #            ]
-    #        )"""
+    for i in match_lines:
+        print(i)
 
     # Why would invert return a 1 on matches? Isnt exit-code "decoupled" from logic? A '1' signifies an error or no matches tbh.
     # if len(match_lines) == 0:
