aa57380401ffd26e462b6f148c7cd0df91ad08f9
Andreas Brandting <andreas@brandting.com>
2024-03-15 16:28:28 +0100

    [WIP] Grunden färdig.

--- greppo_logic.py
+++ greppo_logic.py
@@ -1,21 +1,132 @@
-# Skriv endast definitioner här på denna indenteringsnivå! Det är viktigt att du
-# namnger funktioner, klasser och variabler exakt med de namn som står i
-# beskrivningen.
+import os
+import sys
+
+def greppo_logic(search_terms, filenames, invert_match, show_linenumbers):
+    
+    matches = []
+    exit_code = 1
+
+
+    for file in filenames: # Loopar file genom filenames
+        if os.path.exists(file): # Kollar om filen finns (True or False)
+            with open(file, 'r') as content: # Öppnar filen som 'content'
+                
+                for index, line in enumerate(content, start=1): #Ger filen indexering som startar på 1
+                    
+                    line=line.strip() #För att listan matches inte ska få med end och whitespaces
+
+                    if search_terms:
+                        for search in search_terms:
+                            
+                            if invert_match: #Hantering vid inventering
+                                    if search not in line: #kollar om sökorden finns i raderna
+                                        if show_linenumbers: # Kollar om show_linenumbers är True
+                                            matches.append(f"{file}:{index}:{line}") 
+                                            exit_code = 0
+                                            
+                                        else:
+                                            matches.append(f"{file}:{line}")
+                                            exit_code = 0
+                                            
+
+                            else: #Hanterar sökningen i fallen där den inte ska inverteras. 
+                                if search in line:    
+
+                                    if show_linenumbers:
+
+                                        matches.append(f"{file}:{index}:{line}")
+                                        exit_code = 0
+
+                                    else: 
+                                        matches.append(f"{file}:{line}")
+                                        exit_code = 0
+                    
+                return matches, exit_code
+
+        else:
+            print("File does not exist")
+
+                            
+                            
+                            
+                            
+                                
+                
+
+                                  
+                                
+
+                            
+                            
+                            
+                                    
+
+
+                                            
+                                        
+                                
+                
+                    
+                
+        
+
+
+            
+                
+    
+    
+    
+    
+    
+    
+    #2: SÖK EFTER EN STRÄNG I EN FIL
+
+    #3: SÖKA EFTER FLERA STRÄNGAR I FLERA FILER MED RADNUMMER
+
+    #4: VISA ALLA RADER SOM INTE INNEHÅLLER EN VISS TEXTSTRÄNG
+
+
+    
+    
+    
+    
+    
+    
+    
+    #5: TYST LÄGE (BORDE SKRIVAS DIREKT I ARGS)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """
+    1:  search_terms: Lista med strängar att söka efter.
+    
+    2:  filenames: Lista med filnamn att söka i.
+    
+    3:  invert_match: Boolsk flagga för om sökningen ska inverteras.
+    
+    4:  show_linenumbers: Boolsk flagga för om radnummer ska finnas med i resultatet.
+    
+    5:  Funktionen ska returnera en tupel (exit_code, matches), där exit_code är 0 om någon match hittades (eller vid inverterad sökning, om någon rad inte matchade)
+    , annars 1. matches är en lista med strängar som representerar de matchande raderna.
+   
+     """
+    
+    
+    
+    
+
 
 if __name__ == "__main__":
-    # Här kan du skriva testkod som bara körs när du kör filen direkt och inte
-    # när den importeras som modul i en annan fil.
-    #
-    # Koden importeras som en modul av autograding-funktionen för att utföra ett
-    # "smoke test" av din funktion, så det är viktigt att din kod inte kör något
-    # utanför denna if-sats.
-    #
-    # Exempel:
-    #
-    # print(funktionsnamn("hejsan", 99))
-    # print(funktionsnamn([19, 22, 31, 29, 1])
-    #
-    # Exempel:
-    # minklass = Klass()
-    # print(klass.leet("hejsan")
-    pass  # Ta bort denna rad när du skriver din kod
+    greppo_logic()
