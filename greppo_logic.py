bb15ae785df8363e120479041b675e8b613313e7
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-15 16:25:33 +0100

    Bugfixing duplicate lines

--- greppo_logic.py
+++ greppo_logic.py
@@ -7,13 +7,22 @@
 # Extra - Relative filepaths? ~, ., ..?
 # Extra - -r flag for recursive filesearch?
 
+# Todo - Lines with multiple matches repeat...use a set?
+
 import re
+from collections import Counter
 
 
 def greppo_logic(search_terms, filenames, invert_match: bool, show_line_numbers: bool) -> tuple:
     """Returns lines matching strings in search_terms."""
 
     match_lines = list()
+
+    # This evaluates to True if search_terms is not empty...but is it more correct to do len(search_terms) >= 1 for more readability?
+    if search_terms:
+        pass
+    else:
+        search_terms = ''
 
     for file in filenames:
         try:
@@ -39,17 +48,34 @@ def greppo_logic(search_terms, filenames, invert_match: bool, show_line_numbers:
                     #        search_word_dict[key] = True
 
                     # Returns False if any value in search_word_dict is True. Check length of string to not add empty strings. (\n counts as a char)
-                    if not any(search_word_dict.values()) and len(line) > 1:
+                    if not any(search_word_dict.values()) and len(line) > 1 and line not in match_lines:
                         match_lines.append(rf"{file}:{str(index) + ":" if show_line_numbers else ''}{line.strip()}")
 
             else:
-                match_lines.extend(
-                    [
-                        f"{file}:{str(index) + ":" if show_line_numbers else ''}{line.strip('\n')}"
-                        for x in search_terms
-                        if re.search(rf"\b{x}\b", line)
-                    ]
-                )
+                print("LINE: ", line.strip())
+                print("MATCH: ", match_lines)
+                for i in search_terms:
+                    if line.strip() in match_lines:
+                        print("TRUE")
+                    if re.search(rf"\b{i}\b", line):
+                        match_lines.append(f"{file}:{str(index) + ":" if show_line_numbers else ''}{line.strip('\n')}")
+                #match_lines.extend(
+                #    [
+                #        f"{file}:{str(index) + ":" if show_line_numbers else ''}{line.strip('\n')}"
+                #        for x in search_terms
+                #        if re.search(rf"\b{x}\b", line) and line 
+                #    ]
+                #)
+
+    """It's never gonna match...because .strip() changes the string. And match_lines has the filename etc. in it."""
+
+    """I guess this is needed because the list comprehension only checks the validity on the first loop or something?"""
+    """Removing repeat lines from match_lines"""
+    # .count() vs Counter https://stackoverflow.com/a/23909767
+    #match_count = Counter(match_lines)
+    #for key, value in match_count.items():
+    #    if value != 1:
+    #        match_lines.remove(key)
 
     # Why would invert return a 1 on matches? Isnt exit-code "decoupled" from logic? A '1' signifies an error or no matches tbh.
     if len(match_lines) == 0:
