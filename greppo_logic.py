43cf4edbff0c2fd382a5526ff8fee1ab0fddafd5
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-03-15 19:00:53 +0100

    Refactoring

--- greppo_logic.py
+++ greppo_logic.py
@@ -8,9 +8,6 @@
 # Extra - Relative filepaths? ~, ., ..?
 # Extra - -r flag for recursive filesearch?
 
-# Refactoring - Do I actually need a dict to keep track of matches in the invert case?...If even one value is True, break that iteration without an append.
-#             - Without the dict I would have to check match_lines for repeat lines for every search_term?
-
 import re
 
 
@@ -37,20 +34,19 @@ def greppo_logic(search_terms, filenames, invert_match: bool, show_line_numbers:
         for index, line in enumerate(reader):
             # The full formated string to append to match_lines.
             full_line = f"{file}:{str(index) + ":" if show_line_numbers else ''}{line.strip('\n')}"
-            
+
+            # Flip keys to True if match is found.
+            search_word_dict = {key:True for key in search_terms if re.search(rf"\b{key}\b", line)}
+
             if invert_match:
 
-                    # Flip keys to True if match is found.
-                    search_word_dict = {key:True for key in search_terms if re.search(rf"\b{key}\b", line)}
-
-                    # Returns False if any value in search_word_dict is True. Check length of string to not add empty strings. (\n counts as a char)
-                    if not any(search_word_dict.values()) and len(line) > 1:
-                        match_lines.append(full_line)
+                # Returns False if any value in search_word_dict is True. Check length of string to not add empty strings. (\n counts as a char)
+                if not any(search_word_dict.values()) and len(line) > 1:
+                    match_lines.append(full_line)
 
             else:
-                for i in search_terms:
-                    if re.search(rf"\b{i}\b", line) and full_line not in match_lines:
-                        match_lines.append(full_line)
+                if any(search_word_dict.values()) and len(line) > 1:
+                    match_lines.append(full_line)
 
     # Why would invert return a 1 on matches? Isnt exit-code "decoupled" from logic? A '1' signifies an error or no matches tbh.
     if len(match_lines) == 0:
